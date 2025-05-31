a1efdff858319601a3debe75bdd99b41a811c5ac
Nahid <nahid.web.creator@gmail.com>
2025-05-31 20:18:34 +0600

    navbars background color changed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = () => {
 
   const toggleDarkMode = () => setDarkMode(!darkMode)
   return (
-    <nav className="bg-white relative dark:bg-gray-900 shadow-md p-4 md:sticky top-0 z-20 transition-colors duration-300">
+    <nav className="bg-white relative dark:bg-gray-800 shadow-md p-4 md:sticky top-0 z-20 transition-colors duration-300">
       <div className="container md:flex mx-auto hidden justify-end sm:justify-between items-center">
         {/* <h1 className=" hidden sm:block text-2xl font-bold text-blue-600 dark:text-blue-400">Shahriar Sany</h1> */}
         <h1 className=" hidden sm:block text-2xl font-bold text-gray-600 dark:text-white">{"{S}"}</h1>
@@ -67,7 +67,7 @@ const Navbar = () => {
               <BiMenu onClick={toggleMenu} size={30} />
           }
         </div>
-        <div className={`space-x-4 flex items-center absolute left-0 w-full  border-y border-gray-400 dark:bg-gray-900 bg-white z-10 transition-all overflow-hidden bottom-0 translate-y-full 
+        <div className={`space-x-4 flex items-center absolute left-0 w-full  border-y border-gray-400 dark:bg-gray-800 bg-white z-10 transition-all overflow-hidden bottom-0 translate-y-full 
           ${showMenu ? 'p-4' : 'py-0 h-0 border-0'}
           `}>
           <Link
